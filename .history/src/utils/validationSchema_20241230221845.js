3512c881ff7ef93914eed6d5a6cb146539189039
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-12-30 22:25:25 +0300

    add validation schema

---
+++ .history/src/utils/validationSchema_20241230221845.js
@@ -0,0 +1,15 @@
+import * as Yup from 'yup';
+const newUserSchema = Yup.object().shape({
+  name: Yup.string().required('Zorunlu Alan'),
+  surname: Yup.string().required('Zorunlu Alan'),
+  phoneNumber: Yup.string()
+    .max(20, 'Max 20')
+    .min(11, 'Min 11')
+    .required('Zorunlu Alan'),
+  email: Yup.string()
+    .email('Geçerli Bir E-Posta Adresi Girin')
+    .required('Zorunlu Alan'),
+  gender: Yup.string().required('Zorunlu Alan'),
+  age: Yup.number().min(18, 'Min 18').max(80, 'Max 80'),
+});
+export default newUserSchema;
---
+++ src/utils/validationSchema.js
@@ -0,0 +1,6 @@
+import * as Yup from 'yup';
+
+const NewListSchema = Yup.object().shape({
+  title: Yup.string().required('Zorunlu Alan'),
+});
+export {NewListSchema};
