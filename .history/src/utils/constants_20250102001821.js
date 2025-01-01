a6d3f85b35728e72cc2acd72b6c0bf21db332640
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-01-02 00:19:08 +0300

    Add services

---
+++ .history/src/utils/constants_20250102001821.js
@@ -0,0 +1,6 @@
+import {Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+const API_KEY = 'e5ee00a6b571b766d7c72eb8b2729a1f';
+export {width, height};
--- .history/src/utils/constants_20250102001833.js
+++ .history/src/utils/constants_20250102001833.js
@@ -5,4 +5,4 @@ const {width, height} = Dimensions.get('window');
 const API_KEY = 'e5ee00a6b571b766d7c72eb8b2729a1f';
 const token =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNWVlMDBhNmI1NzFiNzY2ZDdjNzJlYjhiMjcyOWExZiIsIm5iZiI6MTcwNzY1NDE5My4yMzUwMDAxLCJzdWIiOiI2NWM4YmMzMTY4MGJlODAxN2VhNTMzNjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.qmJzjm84_P7uwI8JkrkJiEJY3oTvdh6g0B1490oDkz0';
-export {width, height, API_KEY, token};
+export {width, height};
