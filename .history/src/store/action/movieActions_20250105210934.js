bda07a605de898273aa3cb5fca734d7337f1adc3
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-01-05 21:09:58 +0300

    updated

---
+++ .history/src/store/action/movieActions_20250105210934.js
@@ -0,0 +1,15 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {getRequest} from '../../service/verbs';
+import {TOP_RATED_MOVIE_URL} from '../../service/url';
+
+const getTopRatedMovies = createAsyncThunk(
+  'movies/getTopRatedMovies',
+  async params => {
+    const response = await getRequest(TOP_RATED_MOVIE_URL, params);
+
+    console.log(response.data.results);
+
+    return response.data.results;
+  },
+);
+export {getTopRatedMovies};
